dea2d5571a6a0c1196c0bcb58b08ffa725b53c58
Patrick Goulding <patrickgoulding@gmail.com>
2024-11-21 14:50:41 -0800

    Basic backend setup

--- backend/go.mod
+++ backend/go.mod
@@ -1,4 +1,4 @@
-module github.com/TutorialEdge/realtime-chat-go-react
+module github.com/pgoulding/go-chat
 
 go 1.19
 
--- backend/main.go
+++ backend/main.go
@@ -2,18 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func setupRoutes() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Simple Server")
+		fmt.Println("Home")
+		fmt.Fprintln(w, "Simple Server")
 	})
+
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request){
+		fmt.Println("Test")
+		fmt.Fprintln(w, "Test Page")
+	})
+
+	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request){
+		fmt.Println("API")
+		fmt.Fprintln(w, "Logic handling goes here")
+	})
+
 }
 
 func main() {
 
 	fmt.Println("Chat App V0.01")
 	setupRoutes()
-	http.ListenAndServe(":8080", nil)
+	log.Println("Starting server on :4000")
+	log.Fatal(http.ListenAndServe(":4000", nil))
 }
